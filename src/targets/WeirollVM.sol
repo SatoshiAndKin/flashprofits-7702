730d0d8d23c4a08226d804bd7ef799405374a5b8
Bryan Stitt <bryan@stitthappens.com>
2026-01-07 15:09:44 -0800

    i hate having to git add. this is pushing me to jj sooner than i expected

---
+++ src/targets/WeirollVM.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import {VM} from "weiroll-foundry/VM.sol";
+
+contract WeirollVM is VM {
+    error NotDelegateCall();
+
+    // TODO: this delegate pattern works, but we should compare gas to doing `msg.sender == address(this)`
+    address immutable ORIGINAL;
+
+    constructor() {
+        ORIGINAL = address(this);
+    }
+
+    function execute(bytes32[] memory commands, bytes[] memory state) external payable{
+        if (address(this) == ORIGINAL) {
+            revert NotDelegateCall();
+        }
+
+        _execute(commands, state);
+    }
+}
